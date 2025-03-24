696aa9ec1b202924f967f7961df614acdaca9de6
feathercyc <feathercyc@163.com>
2025-03-24 17:40:16 +0000

    compromise.sh 将替换C++防止hexo下划线样式显示错误

--- compromise.sh
+++ compromise.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
-# 替换 ../img/ 为 /img/
 replace() {
+  # 替换图片路径
   find source/_posts -type f -name "*.md" -exec sed -i 's|(\.\./img/|(/img/|g' {} +
+  # 替换 C++ 为 C&#43;&#43;
+  find source/_posts -type f -name "*.md" -exec sed -i 's/ C++/ C\&#43;\&#43;/Ig' {} +
+  # 替换 CPP 为 C&#43;&#43;
+  find source/_posts -type f -name "*.md" -exec sed -i 's/ CPP/ C\&#43;\&#43;/Ig' {} +
+
   echo "替换完成"
 }
 
-# 复原 /img/ 为 ../img/
 restore() {
+  # 复原图片路径
   find source/_posts -type f -name "*.md" -exec sed -i 's|(/img/|(\.\./img/|g' {} +
+  # 复原 C&#43;&#43; 为 C++
+  find source/_posts -type f -name "*.md" -exec sed -i 's/ C\&#43;\&#43;/ C++/g' {} +
+
   echo "复原完成"
 }
 
